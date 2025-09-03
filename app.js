b19eb7c19d6019a7129601dfa69f8428d1962af1
Escobar <escobar@matias.com>
2025-09-02 21:00:59 -0300

    Modificado app.js

--- app.js
+++ app.js
@@ -19,10 +19,6 @@ app.use(cors({
 }));
 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'pug');
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
